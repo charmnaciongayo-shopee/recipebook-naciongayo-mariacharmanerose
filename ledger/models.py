ff0cb38e466383331d130d1057eba72ef4deabb3
charmnaciongayo-shopee <maria.naciongayo@shopee.com>
2024-03-19 23:16:39 +0800

    Added password reset functionality

--- ledger/models.py
+++ ledger/models.py
@@ -2,8 +2,6 @@ from datetime import datetime
 from django.db import models
 from django.urls import reverse
 
-
-# Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
--- ledger/urls.py
+++ ledger/urls.py
@@ -3,7 +3,6 @@ from .views import *
 from .models import *
 from django.conf.urls import include
 
-
 urlpatterns = [
     path('list', RecipeListView.as_view(), name='recipe_list'),
     path('<int:pk>/detail', RecipeDetailView.as_view(), name = 'recipe_details'),
